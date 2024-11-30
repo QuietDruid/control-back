244fa477cfbd4fb7100d156771b33876cf55eab3
quietdruid <sharpshotboom@gmail.com>
2024-11-29 20:23:01 -0800

    added default postgres

--- controlbackend/controlbackend/settings.py
+++ controlbackend/controlbackend/settings.py
@@ -86,11 +86,20 @@ WSGI_APPLICATION = 'controlbackend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+    # """ 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # } """
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
     }
 }
 
