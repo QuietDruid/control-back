1cdd98e4c8c7ba5871f127272fa81195419f1d7c
quietdruid <sharpshotboom@gmail.com>
2024-11-29 16:57:33 -0800

    added workflow test

--- controlbackend/endpoint/urls.py
+++ controlbackend/endpoint/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('', views.index, name="test_index"),
     path('api/classes/', views.create_class, name='create_class'),
     path('api/classes/list/', views.get_classes, name='get_classes'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
