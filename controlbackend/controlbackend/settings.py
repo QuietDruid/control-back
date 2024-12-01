c54211bf2088463aab255880565e30c4c1519087
quietdruid <sharpshotboom@gmail.com>
2024-11-30 21:44:48 -0800

    some changes

--- controlbackend/controlbackend/settings.py
+++ controlbackend/controlbackend/settings.py
@@ -24,13 +24,13 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
-    'http://10.200.10.50:5173'        
+    'http://10.200.10.50'        
 ]
 
 # Application definition
