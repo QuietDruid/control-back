4e7af6faef9da97ce18f5fd02a36c714a48de2fe
quietdruid <sharpshotboom@gmail.com>
2024-12-01 00:04:58 -0800

    https the reckoning

--- controlbackend/controlbackend/settings.py
+++ controlbackend/controlbackend/settings.py
@@ -26,6 +26,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '10.200.10.50',
